6903e0fadc3c6d19f46e41e9e3e70991bb492a29
naum <inquether@gmail.com>
2026-04-22 18:39:53 +0300

    add telegram alerts

--- monitor.py
+++ monitor.py
@@ -2,18 +2,33 @@ import requests
 import datetime
 import time
 
+TOKEN = "8767610691:AAEMhGRyTnnJjNlH5XR9okAthuGI7oWiZko"
+CHAT_ID = "350857578"
+
 url = "https://google.com"
+
+def send_telegram_message(message):
+    telegram_api_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    requests.post(telegram_api_url, data=data)
 
 while True:
     try:
-        response = requests.get(url)
+        response = requests.get(url,timeout=5)
 
         if response.status_code == 200:
             print(f"{datetime.datetime.now()} | {url} is UP")
         else:
-            print(f"{url} is DOWN. Status Code: {response.status_code}")
+            print(f"🚨 {url} is DOWN. Status Code: {response.status_code}")
+            send_telegram_message(f"🚨 {url} is DOWN. Status Code: {response.status_code}")
 
     except Exception as e:
         print("CRITICAL ERROR", e)
+        send_telegram_message(f"🔥 CRITICAL ERROR: {e}")
 
-    time.sleep(60)
+    time.sleep(10)
