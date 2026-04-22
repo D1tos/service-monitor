b2e56df42dffbc9a1e8cc5be35704a1fade65da5
naum <inquether@gmail.com>
2026-04-22 12:10:08 +0300

    add http request with error handling

--- monitor.py
+++ monitor.py
@@ -1,1 +1,9 @@
-print("monitor v2")
+import requests
+
+url = "https://google.com"
+
+try:
+    response = requests.get(url)
+    print(response.status_code)
+expect Exception as e:
+    print("Ошибка", e)
